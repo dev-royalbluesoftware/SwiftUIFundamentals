e83960c565ef3eadb7f84332830f0e0bbabfb6be
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-02-19 20:35:34 -0700

    Add CodeExamples, GroupBox and CardView

---
+++ SwiftUI-CodeExamples/SwiftUI-CodeExamples/SwiftUI_CodeExamplesApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// SwiftUI-CodeExamples
+// SwiftUI_CodeExamplesApp.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+@main
+struct SwiftUI_CodeExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
