ef5f18d08d721d3bc5aacb6d731cf86799f188a8
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-02-27 23:21:54 -0700

    Refactor YouAreAwesome, update sound and audioPlayer to prevent potential errors.

--- BC-SwiftUI/01_YouAreAwesome/YouAreAwesome/ContentView.swift
+++ BC-SwiftUI/01_YouAreAwesome/YouAreAwesome/ContentView.swift
@@ -20,6 +20,8 @@ struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
     @State private var soundName = ""
     
+    @State private var soundIsOn = true
+    
     // MARK: - Properties
     
     let numberOfImages = 10
@@ -45,52 +47,86 @@ struct ContentView: View {
             
             Spacer()
             
-            Button {
-                let messages = ["You Are Awesome!",
-                                "When the Genuis Bar Needs Help, They Call You!",
-                                "You Are Amazing!",
-                                "You Are Great!",
-                                "You Are Incredible!",
-                                "You Are Superb!",
-                                "You Are Fantastic!",
-                                "You Are Mind Blowing!",
-                                "You Are Radiant!"]
-                
-                var lastMessageString: String
-                var lastImageName: String
-                var lastSoundName: String
-
-                repeat {
-                    lastMessageString = messages.randomElement() ?? "You Are Awesome!"
-                    lastImageName = "image\(Int.random(in: 0...numberOfImages - 1))"
-                    lastSoundName = "sound\(Int.random(in: 0...numberOfSounds - 1))"
-                } while lastMessageString == messageString || lastImageName == imageName || lastSoundName == soundName
-                
-                messageString = lastMessageString
-                imageName = lastImageName
-                soundName = lastSoundName
-                
-                // Audio Player
-                guard let soundFile = NSDataAsset(name: soundName) else {
-                    print("😡 Could not read file named \(soundName)")
-                    return
-                }
-                
-                do {
-                    audioPlayer = try AVAudioPlayer(data: soundFile.data)
-                    audioPlayer.play()
+            HStack {
+                Text("Sound On:")
                     
-                } catch {
-                    print("😡 ERROR: \(error.localizedDescription) creating audio player")
+                Toggle("", isOn: $soundIsOn)
+                    .labelsHidden()
+                    .onChange(of: soundIsOn) {
+                        if audioPlayer != nil && audioPlayer.isPlaying {
+                            audioPlayer.stop()
+                        }
+                    }
+                
+                Spacer()
+                
+                Button {
+                    let messages = ["You Are Awesome!",
+                                    "When the Genuis Bar Needs Help, They Call You!",
+                                    "You Are Amazing!",
+                                    "You Are Great!",
+                                    "You Are Incredible!",
+                                    "You Are Superb!",
+                                    "You Are Fantastic!",
+                                    "You Are Mind Blowing!",
+                                    "You Are Radiant!"]
+                    
+                    var lastMessageString: String
+                    var lastImageName: String
+                    var lastSoundName: String
+                    
+                    repeat {
+                        lastMessageString = messages.randomElement() ?? "You Are Awesome!"
+                        lastImageName = "image\(Int.random(in: 0..<numberOfImages))"
+                        lastSoundName = "sound\(Int.random(in: 0..<numberOfSounds))"
+                    } while lastMessageString == messageString || lastImageName == imageName || lastSoundName == soundName
+                    
+                    messageString = lastMessageString
+                    imageName = lastImageName
+                    soundName = lastSoundName
+                    
+                    if soundIsOn {
+                        playSound()
+                    }
+                    
+                } label: {
+                    Text("Show Message")
                 }
-            } label: {
-                Text("Show Message")
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.indigo)
             }
-            .buttonStyle(.borderedProminent)
-            .font(.title2)
-            .tint(.indigo)
         }
         .padding()
+    }
+    
+    // MARK: - Helper Methods
+    
+    func nonRepeatingRandomIndex(lastNumber: Int, upperBound: Int) -> Int {
+        var newNumber: Int
+        repeat {
+            newNumber = Int.random(in: 0..<upperBound)
+        } while newNumber == lastNumber
+        return newNumber
+    }
+    
+    func playSound() {
+        if audioPlayer != nil && audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
+        
+        // Audio Player
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("😡 Could not read file named \(soundName)")
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription) creating audio player")
+        }
     }
 }
 
