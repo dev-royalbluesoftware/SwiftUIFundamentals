fa894e8c8ee3f01bb4126032035e53d32420cc17
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-03-03 18:38:15 -0700

    WordGarden App initial commit

---
+++ BC-SwiftUI/02_WordGarden/WordGarden/WordGardenApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// WordGarden
+// WordGardenApp.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
