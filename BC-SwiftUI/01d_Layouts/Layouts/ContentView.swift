1aad44b173b5de2a084508a35a17abaa3e0643f4
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-03-05 19:52:44 -0700

    Layouts complete

--- BC-SwiftUI/01d_Layouts/Layouts/ContentView.swift
+++ BC-SwiftUI/01d_Layouts/Layouts/ContentView.swift
@@ -5,40 +5,62 @@
 //
 // Created by rbs-dev
 // Copyright © Royal Blue Software
-// 
+//
 
 
 import SwiftUI
 
 struct ContentView: View {
-
+    
     // MARK: - @State Properties
     
     @State private var messageString = ""
-
+    
     var body: some View {
-        VStack {
-            Text(messageString)
-                .font(.largeTitle).fontWeight(.heavy)
-                .foregroundStyle(.red)
-                .minimumScaleFactor(0.8)
-                .multilineTextAlignment(.center)
-                .frame(height: 250)
-                .frame(maxWidth: .infinity)
+        GeometryReader { geometry in
             
-            HStack {
-                Button("Awesome") {
-                    messageString = "You Are Awesome!"
-                }
+            VStack {
+                Text("You Have Skills!")
+                    .font(.largeTitle).fontWeight(.black)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.maroonBC)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
                 
-                Button("Great") {
-                    messageString = "You Are Great!"
+                Spacer()
+                
+                Text(messageString)
+                    .font(.largeTitle).fontWeight(.heavy)
+                    .foregroundStyle(.red)
+                    .minimumScaleFactor(0.8)
+                    .multilineTextAlignment(.center)
+                    .frame(height: 250)
+                    .frame(maxWidth: .infinity)
+                
+                Spacer()
+                
+                Divider()
+                    .background(.black)
+                
+                Rectangle()
+                    .fill(.white)
+                    .frame(width: geometry.size.width * 0.75, height: 1)
+                
+                HStack {
+                    Button("Awesome") {
+                        messageString = "You Are Awesome!"
+                    }
+                    
+                    Button("Great") {
+                        messageString = "You Are Great!"
+                    }
                 }
+                .buttonStyle(.borderedProminent)
+                .tint(.indigo)
             }
-            .buttonStyle(.borderedProminent)
-            .tint(.indigo)
+            .padding()
+            .background(.skyBlue.gradient)
         }
-        .padding()
     }
 }
 
