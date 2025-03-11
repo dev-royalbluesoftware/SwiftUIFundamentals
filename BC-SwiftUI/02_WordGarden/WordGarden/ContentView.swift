eca1347541452f8f456142ea1292faa52905511f
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-03-10 22:45:42 -0600

    WordGarden initial interface setup

--- BC-SwiftUI/02_WordGarden/WordGarden/ContentView.swift
+++ BC-SwiftUI/02_WordGarden/WordGarden/ContentView.swift
@@ -5,20 +5,87 @@
 //
 // Created by rbs-dev
 // Copyright © Royal Blue Software
-// 
+//
 
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - @State Properties
+    
+    @State private var wordsGuessed = 0
+    @State private var wordsMissed = 0
+    @State private var wordsToGuess = ["Swift", "UI", "Kit", "View", "SwiftUI", "SwiftData", "CoreData", "VisionOS", "MacOS"]
+    @State private var gameStatusMessage = "How Many Guesses to Uncover the Hidden Word?"
+    @State private var currentWord = 0 // index in wordsToGuess array
+    @State private var guessedLetter = ""
+    @State private var imageName = "flower8"
+    @State private var playAgainHidden = true
+    
+    // MARK: -
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Words Guessed: \(wordsGuessed)")
+                    
+                    Text("Words Missed: \(wordsMissed)")
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("Words to Guess: \(wordsToGuess.count - wordsGuessed + wordsMissed)")
+                    
+                    Text("Words in Game: \(wordsToGuess.count)")
+                }
+            }
+            .padding(.horizontal)
+            
+            Spacer()
+            
+            Text(gameStatusMessage)
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            // TODO: Switch to wordsToGuess[currentWord]
+            Text("_ _ _ _ _ _")
+                .font(.title)
+            
+            if playAgainHidden {
+                HStack {
+                    TextField("", text: $guessedLetter)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 30).overlay {
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.gray, lineWidth: 1)
+                        }
+                    
+                    Button("Guess a Letter") {
+                        // TODO: Guess a Letter Button action here...
+                    }
+                    .buttonStyle(.bordered)
+                    .tint(.mint)
+                }
+                
+            } else {
+                Button("Another Word") {
+                    // TODO: Another Word Button Action Here...
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            }
+            
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
+        .ignoresSafeArea(edges: .bottom)
     }
 }
 
