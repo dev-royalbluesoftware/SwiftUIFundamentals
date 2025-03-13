47b374e273dda6c029181edfec6cdbe9ff2907dd
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-03-12 21:55:30 -0600

    Bip The Guy initial commit

---
+++ BC-SwiftUI/03_BipTheGuy/BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// BipTheGuy
+// BipTheGuyApp.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
